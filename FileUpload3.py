e8d615a8adbcf2b66b5009868104db50b37f48b3
Div Patil <divbpatil123@gmail.com>
2023-10-18 13:29:33 +0530

    Line 12 and 13 added.

--- FileUpload3.py
+++ FileUpload3.py
@@ -9,3 +9,5 @@ driver = webdriver.Chrome(options=chrome_options)
 
 driver.get('https://the-internet.herokuapp.com/upload')
 driver.maximize_window()
+driver.find_element(By.ID, 'file-upload').send_keys("C:\\Users\\Lenov\\Desktop\\File1.docx")
+driver.find_element(By.XPATH,'//*[@id="file-submit"]').click()
